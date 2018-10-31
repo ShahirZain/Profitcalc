2c577f2e9e876d0b32aa9a1d69c3e1d16158b025
shahirzain <shahirzain100@gmail.com>
2018-11-01 01:01:14 +0500

    final

---
+++ STM/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace STM
+{
+    public partial class Form1 : Form
+    {
+        conn conn = new conn();
+       
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+
+        }
+
+
+        public void loadData() {
+            conn.sqlConnection1.Open();
+            SqlCommand cmd = new SqlCommand("Select * from SM",conn.sqlConnection1);
+            SqlDataReader dr = cmd.ExecuteReader();
+            DataTable dt = new DataTable();
+            dt.Load(dr);
+            this.dataGridView1.DataSource = dt;
+            conn.sqlConnection1.Close();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            conn.sqlConnection1.Open();
+            try
+            {
+                SqlCommand cmd = new SqlCommand("insert into SM(Product_Name,Cost,Sell,Profit,DateTime) values(@Product_Name,@Cost,@Sell,@Profit,@DateTime)", conn.sqlConnection1);
+                cmd.Parameters.AddWithValue("@Product_Name", this.textBox1.Text);
+                cmd.Parameters.AddWithValue("@Cost", Convert.ToInt32(this.textBox2.Text));
+                cmd.Parameters.AddWithValue("@Sell", Convert.ToInt32(this.textBox3.Text));
+                cmd.Parameters.AddWithValue("@Profit", (-1)*(Convert.ToInt32(this.textBox2.Text)- Convert.ToInt32(this.textBox3.Text)));
+                cmd.Parameters.AddWithValue("@DateTime", dateTimePicker1.Value.ToString());
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            conn.sqlConnection1.Close();
+            MessageBox.Show("Data submitted");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            loadData();
+        }
+    }
+}
---
+++ STM/conn.Designer.cs
@@ -0,0 +1,54 @@
+﻿namespace STM
+{
+    partial class conn
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.sqlConnection1 = new System.Data.SqlClient.SqlConnection();
+            this.SuspendLayout();
+            // 
+            // sqlConnection1
+            // 
+            this.sqlConnection1.ConnectionString = "Data Source=DESKTOP-ES52MLE;Initial Catalog=store;Integrated Security=True";
+            this.sqlConnection1.FireInfoMessageEventOnUserErrors = false;
+            // 
+            // conn
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "conn";
+            this.Text = "conn";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        public System.Data.SqlClient.SqlConnection sqlConnection1;
+    }
+}
